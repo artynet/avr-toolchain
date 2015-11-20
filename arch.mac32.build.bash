35cd2a1111407af9a8989dc3c2ba0a8aaccbbb72
artynet <arty.net2@gmail.com>
2015-11-20 19:06:39 +0100

    fixing avrdude version to 6.1

--- arch.mac32.build.bash
+++ arch.mac32.build.bash
@@ -23,5 +23,5 @@ rm -f *arduino*.tar.bz2 *arduino*.zip
 
 tar -cjvf ./avr-gcc-4.8.1-arduino5-i386-apple-darwin11.tar.bz2 avr/
 
-tar -cjvf ./avrdude-6.0.1-arduino5-i386-apple-darwin11.tar.bz2 avrdude-6.0.1/
+tar -cjvf ./avrdude-6.1-arduino5-i386-apple-darwin11.tar.bz2 avrdude-6.1/
 
